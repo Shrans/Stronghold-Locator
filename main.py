d10f3f3693dfc7df45751a6c033b521614912dd4
lidongxun967 <89326292+lidongxun967@users.noreply.github.com>
2023-07-01 21:08:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,32 +10,32 @@ import LocatorLib
 
 
 class MainWindow(QMainWindow):
-    def __init__(self, parent = None) :
+    def __init__(self, parent = 无) :
         
-        super().__init__(parent)
+        super()。__init__(parent)
         self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-        self.ui.dx_jd.clicked.connect(self.dx_jd)
-        self.ui.dx_gs.clicked.connect(self.dx_gs)
-        self.ui.about.clicked.connect(self.about)
+        self.ui。setupUi(self)
+        self.ui。dx_jd。clicked。connect(self.dx_jd)
+        self.ui。dx_gs。clicked。connect(self.dx_gs)
+        self.ui。关于。clicked。connect(self.关于)
     
 
     def dx_jd(self):
-        xyt_1=str(self.ui.xyt_1.text())
-        xyt_2=str(self.ui.xyt_2.text())
-        self.ui.print_out.setText(LocatorLib.Calculate(xyt_1,xyt_2))
+        xyt_1=str(self.ui。xyt_1。text())
+        xyt_2=str(self.ui。xyt_2。text())
+        self.ui。print_out。setText(LocatorLib.Calculate(xyt_1,xyt_2))
 
     def dx_gs(self):
-        xyt_1=str(self.ui.xyt_1.text())
-        self.ui.print_out.setText(LocatorLib.Estimate(xyt_1))
+        xyt_1=str(self.ui。xyt_1。text())
+        self.ui。print_out。setText(LocatorLib.Estimate(xyt_1))
 
-    def about():
-        webbrowser.open("github.com/lidongxun967/Stronghold-Locator-GUI")
+    def 关于():
+        webbrowser.已打开("github.com/lidongxun967/Stronghold-Locator-GUI")
 
  
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     win = MainWindow()
-    win.setWindowTitle("Test")
-    win.show()
+    win.setWindowTitle("Stronghold-Locator")
+    win.显示()
     app.exit(app.exec_())
